b233e207f3410ef594a3df299da348998ffeb619
nvlannasik <annasikn@yahoo.com>
2022-10-13 14:46:24 +0700

    fix endpoint & schema product

--- models/Product.js
+++ models/Product.js
@@ -4,46 +4,49 @@ require("dotenv");
 let connection = mongoose.createConnection(process.env.DB_CONNECTION);
 autoIncrement.initialize(connection);
 
-const ProductSchema = mongoose.Schema({
-  name : {
-    type: String,
-    required: true,
-    min: 6,
-    max: 25,
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 25,
+    },
+    description: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 25,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    harvestDate: {
+      type: String,
+      required: true,
+    },
+    expirationDate: {
+      type: String,
+      required: true,
+    },
+    timestamps: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description : {
-    type: String,
-    required: true,
-    min: 6,
-    max: 25,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  stock: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  harvestDate: {
-    type: String,
-    required: true,
-  },
-  expirationDate: {
-    type: String,
-    required: true,
-  },
-  timestamps: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 ProductSchema.plugin(autoIncrement.plugin, "Product");
 module.exports = mongoose.model("Product", ProductSchema);
